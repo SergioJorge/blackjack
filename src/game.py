df42b75269097e7ff5cfb946ff76a16295134d32
Pedro Assis <pedroh2306@gmail.com>
2013-07-17 17:05:32 -0300

    Game Working

--- src/game.py
+++ src/game.py
@@ -1,4 +1,52 @@
+import random
+import sys
+
 class Game(object):
 
     def __init__(self):
-        pass
+        self.cards = [[self.getCard()], [self.getCard(), self.getCard()]]
+
+    def getCard(self):
+        random.seed()
+        return random.choice(range(2,11))
+
+    def hit(self):
+        self.cards[1].append(self.getCard())
+
+    def stand(self):
+        player_points = sum(self.cards[1])
+
+        while(player_points >= sum(self.cards[0])):
+            self.cards[0].append(self.getCard())
+
+        if (self.lost(self.cards[0])):
+            print "Player wins"
+        else:
+            print "House wins"
+        self.printStatus()
+        sys.exit(0)
+                
+    def printStatus(self):
+        print "Dealer: ", self.cards[0], sum(self.cards[0])
+        print "Player: ", self.cards[1], sum(self.cards[1])
+
+    def lost(self, hands):
+        return sum(hands) > 21        
+
+    def getAction(self):
+        action = raw_input("Stand [S], Hit [H]? ")
+        if action == "S":
+            self.stand()
+        elif action == "H":
+            self.hit()
+            if self.lost(self.cards[1]):
+                print "House wins"
+                self.printStatus()
+                sys.exit(0)
+        
+
+game = Game()
+
+while(True):
+    game.printStatus()
+    game.getAction()
--- test/test_game.py
+++ test/test_game.py
@@ -1,5 +1,33 @@
 import unittest
 
+from game import Game
 
 class GameTestCase(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.game = Game()
+
+    def testShouldStarte(self):
+        self.assertEqual(len(self.game.cards[0]), 1)
+        self.assertEqual(len(self.game.cards[1]), 2)
+
+    def testShouldReceiveCard(self):
+        card = self.game.getCard()
+        self.assertTrue(card >= 2 and card <= 10)
+    
+    def testHit(self):
+        self.game.hit()
+        self.assertEqual(len(self.game.cards[1]), 3)
+ 
+    def testStand(self):
+        self.game.stand()
+        self.assertTrue(sum(self.game.cards[0]) > sum(self.game.cards[1]))
+    
+    def testShouldPrintGameStatus(self):
+        self.game.getStatus()
+
+    def test_lost(self):
+        self.assertFalse(lost([2,10])
+
+if __name__ == '__main__':
+    unittest.main()
